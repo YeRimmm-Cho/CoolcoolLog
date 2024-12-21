5aa86539b038626ca4feea299b44d285fa8c7ee0
YeRimmm-Cho <em514@dgu.ac.kr>
2024-12-21 21:46:19 +0900

    Refacotr: 스키마 버전 변경

--- app/src/main/java/com/example/coolcoollog/data/database/AppDatabase.java
+++ app/src/main/java/com/example/coolcoollog/data/database/AppDatabase.java
@@ -8,7 +8,7 @@ import android.content.Context;
 import com.example.coolcoollog.data.dao.SleepRecordDao;
 import com.example.coolcoollog.data.entity.SleepRecord;
 
-@Database(entities = {SleepRecord.class}, version = 1, exportSchema = false)
+@Database(entities = {SleepRecord.class}, version = 2, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     private static AppDatabase instance;
 
